5c2ad3b17cdc5be9b3c0c1edd59327736659e77a
Denisse Ventura <d.ventura@ufl.edu>
2018-11-03 20:01:09 -0500

    WebCam2

---
+++ Webcam2/Webcam2/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Webcam2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(140, 580);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(737, 33);
+            this.ComboBox1.TabIndex = 0;
+            this.ComboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PictureBox1.Location = new System.Drawing.Point(140, 58);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(1300, 516);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 1;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1562, 816);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.ComboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+    }
+}
+
---
+++ Webcam2/Webcam2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Webcam2
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice VideoSrc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in videoDevices)
+            {
+                ComboBox1.Items.Add(device.Name);
+            }
+            try
+            {
+                VideoSrc = new VideoCaptureDevice(videoDevices[ComboBox1.SelectedIndex].MonikerString);
+            }
+            catch(ArgumentOutOfRangeException )
+            {
+                VideoSrc = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            }
+
+            VideoSrc.NewFrame += new NewFrameEventHandler(VideoSrc_NewFrame);
+            VideoSrc.Start();
+
+        }
+        void VideoSrc_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            try
+            {
+                PictureBox1.Image = image;
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+        private void Form1_Forceclose(object sender, FormClosingEventArgs e)
+        {
+            VideoSrc.Stop();
+        }
+
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VideoSrc.Stop();
+            VideoSrc = new VideoCaptureDevice(videoDevices[ComboBox1.SelectedIndex].MonikerString);
+            VideoSrc.NewFrame += new NewFrameEventHandler(VideoSrc_NewFrame);
+            VideoSrc.Start();
+        }
+    }
+}
