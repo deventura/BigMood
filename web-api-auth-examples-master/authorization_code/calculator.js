05dc9cffec4921e197b33c07e8970b111f9e2be3
Denisse Ventura <d.ventura@ufl.edu>
2018-11-04 02:34:25 -0600

    Good Stuff

--- web-api-auth-examples-master/authorization_code/calculator.js
+++ web-api-auth-examples-master/authorization_code/calculator.js
@@ -1,5 +1,39 @@
-function songCalculator(){
+//Main function that is called after analyzing face
+function assignEmotions(){
+    var fear;
+    var happy;
+    var neutral;
+    var sad;
+    var anger;
 
+
+}
+
+function songCalculator(fear, happy, neutral, sad, anger){
+    var dance;
+    var energy;
+    var mode;
+    var valence;
+
+    dance = (0*fear)+(1*happy)+(0.5*neutral)+(0*sad)+(0*anger);
+    energy = (0*fear)+(1*happy)+(0.5*neutral)+(0*sad)+(1*anger);
+
+    if((happy + neutral + anger) > (fear+sad)){
+        mode = 1;
+    }
+    else{
+        mode = 0;
+    }
+
+    valence = (1*fear)+(1*happy)+(0.5*neutral)+(0*sad)+(0*anger);
+}
+
+function songSelection(dance, energy, mode, valence){
+    const URL = 'https://api.spotify.com/v1/audio-features/06AKEBrKUckW0KREUWRnvT'
+    /*fetch(URL)
+    .then*/
 
     
+
+
 }
