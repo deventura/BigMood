101f08506d142c8175d2af6e175cecccf37edb8e
Denisse Ventura <d.ventura@ufl.edu>
2018-11-04 09:11:15 -0600

    Deleted Folder

--- web-api-auth-examples-master/authorization_code/final.js
+++
@@ -1,9 +0,0 @@
-
-function readFile(){
-    var fso, ts,s;
-    fso = new ActiveXObject("Scripting.FileSystemObject");
-    f1 = fso.OpenTextFile("c:\\EmotionData.txt", 1);
-    s = ts.ReadLine();
-
-    
-}
